8ec68b000f5e78c591bd53f42188075eac9fde86
ardengt <arden@students.amikom.ac.id>
2026-06-08 20:37:43 +0700

    fix: setup rewrites proxy to digitalocean

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // Menambahkan jalur proxy (rewrites) untuk mengatasi Mixed Content 
+  // dari Vercel (HTTPS) ke VPS DigitalOcean (HTTP)
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://159.223.82.138:8080/api/:path*',
+        // Catatan: Pastikan port backend Apomacy benar
+
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
