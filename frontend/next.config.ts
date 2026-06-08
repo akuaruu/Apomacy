abca77f8b232ca5f423487e6040666360679b360
ardengt <arden@students.amikom.ac.id>
2026-06-08 20:42:16 +0700

    fix: endpoint

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://159.223.82.138:8080/api/:path*',
+        destination: 'http://159.223.82.138:8080/api/:path*/',
         // Catatan: Pastikan port backend Apomacy benar
 
       },
